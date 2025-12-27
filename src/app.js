8d24b2c38b11b2c8395319785a9d7b1d3cce8f61
MahePath90300 <mahender.fedev32@gmail.com>
2025-12-27 10:33:14 +0530

    Removed jade file setup

--- src/app.js
+++ src/app.js
@@ -15,9 +15,6 @@ const attendanceRoute = require("./routes/attendance.route");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json({ limit: "10mb" }));
