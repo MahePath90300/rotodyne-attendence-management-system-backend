83098506fab8f06cfcb046ab69399c3efb91cbc0
MahePath90300 <mahender.fedev32@gmail.com>
2026-01-12 12:38:18 +0530

    Production issues fixes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
-const app = require('./app');
-const  connectDB = require('./config/db');
-const { PORT } = require('./config/env');
+require("dotenv").config();
+
+const app = require("./app");
+const connectDB = require("./config/db");
+
+const PORT = process.env.PORT || 5000;
 
 (async function start() {
-  await connectDB();
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("❌ Server startup failed:", err);
+    process.exit(1);
+  }
 })();
